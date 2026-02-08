73d278f4c6c204700628cc382752a42e5dc5f9c5
Hotiovip <hotiovipita@gmail.com>
2026-02-08 01:41:14 +0100

    Fixed warnings

--- src/main/java/org/hotiovip/HPotions.java
+++ src/main/java/org/hotiovip/HPotions.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.Potions;
 import org.hotiovip.effect.*;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,73 +21,73 @@ public class HPotions implements ModInitializer {
 	public static final String MOD_ID = "hpotions";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Holder<MobEffect> ENDER_EFFECT =
+    public static final Holder<@NotNull MobEffect> ENDER_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "ender_eye"), new EnderEffect());
-    public static final Holder<Potion> ENDER_POTION =
+    public static final Holder<@NotNull Potion> ENDER_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "ender_eye"),
                     new Potion("ender_eye", new MobEffectInstance(HPotions.ENDER_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> COPPER_EFFECT =
+    public static final Holder<@NotNull MobEffect> COPPER_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "copper_eye"), new CopperEffect());
-    public static final Holder<Potion> COPPER_POTION =
+    public static final Holder<@NotNull Potion> COPPER_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "copper_eye"),
                     new Potion("copper_eye", new MobEffectInstance(HPotions.COPPER_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> IRON_EFFECT =
+    public static final Holder<@NotNull MobEffect> IRON_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "iron_eye"), new IronEffect());
-    public static final Holder<Potion> IRON_POTION =
+    public static final Holder<@NotNull Potion> IRON_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "iron_eye"),
                     new Potion("iron_eye", new MobEffectInstance(HPotions.IRON_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> GOLD_EFFECT =
+    public static final Holder<@NotNull MobEffect> GOLD_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "gold_eye"), new GoldEffect());
-    public static final Holder<Potion> GOLD_POTION =
+    public static final Holder<@NotNull Potion> GOLD_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "gold_eye"),
                     new Potion("gold_eye", new MobEffectInstance(HPotions.GOLD_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> LAPIS_EFFECT =
+    public static final Holder<@NotNull MobEffect> LAPIS_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "lapis_eye"), new LapisEffect());
-    public static final Holder<Potion> LAPIS_POTION =
+    public static final Holder<@NotNull Potion> LAPIS_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "lapis_eye"),
                     new Potion("lapis_eye", new MobEffectInstance(HPotions.LAPIS_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> REDSTONE_EFFECT =
+    public static final Holder<@NotNull MobEffect> REDSTONE_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "redstone_eye"), new RedstoneEffect());
-    public static final Holder<Potion> REDSTONE_POTION =
+    public static final Holder<@NotNull Potion> REDSTONE_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "redstone_eye"),
                     new Potion("redstone_eye", new MobEffectInstance(HPotions.REDSTONE_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> DIAMOND_EFFECT =
+    public static final Holder<@NotNull MobEffect> DIAMOND_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "diamond_eye"), new DiamondEffect());
-    public static final Holder<Potion> DIAMOND_POTION =
+    public static final Holder<@NotNull Potion> DIAMOND_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "diamond_eye"),
                     new Potion("diamond_eye", new MobEffectInstance(HPotions.DIAMOND_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> EMERALD_EFFECT =
+    public static final Holder<@NotNull MobEffect> EMERALD_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "emerald_eye"), new EmeraldEffect());
-    public static final Holder<Potion> EMERALD_POTION =
+    public static final Holder<@NotNull Potion> EMERALD_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "emerald_eye"),
                     new Potion("emerald_eye", new MobEffectInstance(HPotions.EMERALD_EFFECT, 3600, 0)));
 
-    public static final Holder<MobEffect> ANCIENT_DEBRIS_EFFECT =
+    public static final Holder<@NotNull MobEffect> ANCIENT_DEBRIS_EFFECT =
             Registry.registerForHolder(BuiltInRegistries.MOB_EFFECT, Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "ancient_debris_eye"), new AncientDebrisEffect());
-    public static final Holder<Potion> ANCIENT_DEBRIS_POTION =
+    public static final Holder<@NotNull Potion> ANCIENT_DEBRIS_POTION =
             Registry.registerForHolder(
                     BuiltInRegistries.POTION,
                     Identifier.fromNamespaceAndPath(HPotions.MOD_ID, "ancient_debris_eye"),
@@ -100,111 +101,93 @@ public class HPotions implements ModInitializer {
 		LOGGER.info("HEnchanting loaded!");
 
         // Ender potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    Potions.AWKWARD,
-                    // Ingredient
-                    Items.ENDER_PEARL,
-                    // Output potion.
-                    ENDER_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                Potions.AWKWARD,
+                // Ingredient
+                Items.ENDER_PEARL,
+                // Output potion.
+                ENDER_POTION
+        ));
 
         // Copper potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.COPPER_INGOT,
-                    // Output potion.
-                    COPPER_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.COPPER_INGOT,
+                // Output potion.
+                COPPER_POTION
+        ));
 
         // Iron potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.IRON_INGOT,
-                    // Output potion.
-                    IRON_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.IRON_INGOT,
+                // Output potion.
+                IRON_POTION
+        ));
 
         // Gold potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.GOLD_INGOT,
-                    // Output potion.
-                    GOLD_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.GOLD_INGOT,
+                // Output potion.
+                GOLD_POTION
+        ));
 
         // Lapis potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.LAPIS_LAZULI,
-                    // Output potion.
-                    LAPIS_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.LAPIS_LAZULI,
+                // Output potion.
+                LAPIS_POTION
+        ));
 
         // Redstone potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.REDSTONE,
-                    // Output potion.
-                    REDSTONE_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.REDSTONE,
+                // Output potion.
+                REDSTONE_POTION
+        ));
 
         // Diamond potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.DIAMOND,
-                    // Output potion.
-                    DIAMOND_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.DIAMOND,
+                // Output potion.
+                DIAMOND_POTION
+        ));
 
         // Emerald potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.EMERALD,
-                    // Output potion.
-                    DIAMOND_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.EMERALD,
+                // Output potion.
+                EMERALD_POTION
+        ));
 
         // Ancient debris potion brewing recipes
-        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-            builder.addMix(
-                    // Input potion.
-                    ENDER_POTION,
-                    // Ingredient
-                    Items.NETHERITE_INGOT,
-                    // Output potion.
-                    ANCIENT_DEBRIS_POTION
-            );
-        });
+        FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> builder.addMix(
+                // Input potion.
+                ENDER_POTION,
+                // Ingredient
+                Items.NETHERITE_SCRAP,
+                // Output potion.
+                ANCIENT_DEBRIS_POTION
+        ));
 	}
 }
